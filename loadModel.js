f8d80a2792ee80ef0e71198d4dbe990aba676cc9
jarigantvoort <j.gantvoort@xsarus.nl>
2023-03-26 15:56:21 +0200

    Updated with buttons and inputfields

--- loadModel.js
+++ loadModel.js
@@ -1,22 +1,40 @@
 import { DecisionTree } from "./libraries/decisiontree.js"
 import { VegaTree } from "./libraries/vegatree.js"
 
-function loadSavedModel() {
+let mushroomClass = document.getElementById("mushroomClass");
+let mushroomClassButton = document.getElementById("mushroomClassButton");
+let userInput = document.getElementById("userInput");
+
+
+mushroomClassButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  let value = mushroomClass.value == "" ? "please enter E or P" : mushroomClass.value
+  loadSavedModel(value)
+})
+
+function loadSavedModel(value) {
     fetch("./model.json")
         .then((response) => response.json())
-        .then((model) => modelLoaded(model))
+        .then((model) => modelLoaded(model, value))
 }
 
-function modelLoaded(model) {
+function modelLoaded(model, value) {
     console.log(model)
     let decisionTree = new DecisionTree(model)
 
     // test om te zien of het werkt
-
+    let mushroom = { class: value}
     let prediction = decisionTree.predict(mushroom)
     console.log("predicted " + prediction)
 
+    if(value !== undefined){
+        userInput.innerText = `Your personal prediction was: ${value}, And the computer prediction is: ${prediction}`;
+    }
+
+
     let visual = new VegaTree('#view', 800, 400, decisionTree.toJSON())
+
+
 
 }
 
